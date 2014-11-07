7d80aeaa3a0cf71d933a17d54c429bf63f9fe44d
Allan <allan.callaghan@thisisglobal.com>
2014-11-07 08:13:35 +0000

    Considered web display data handling

--- network_display_event.js
+++ network_display_event.js
@@ -7,49 +7,88 @@ var penatonic_hero = {};
 	}, options) 
 
     var count = 0;
-    var active_note_track = null;
+    
+    // Init --------------------------------------------------------------------
     
     // Init button state arrays for all inputs
     var inputs = Array(options.inputs.length);
     for (var input_number=0 ; input_number < inputs.length ; input_number++) {
-        inputs[input_number] = Array(options.buttons);
+        inputs[input_number] = {} ; //Array(options.buttons);
         var button_states = inputs[input_number];
         for (var button_number=0 ; button_number < button_states.length ; button_number++) {
-            button_states[button_number] = Array();
+            button_states["button_"+button_number] = Array();
         }
+        button_states['previous_active_button'] = null;
     }
     
-    external.tick = function() {
-        count++;
+    // Private -----------------------------------------------------------------
+    
+    function log_track_event(input, button_number, button_state) {
+        /*
+        The last button event is called active_note_track
+        We store this because:
+          - note_off dose not give us the button number
+          - we only EVER play one note at once, so we always know the previous button is the one to disable
+        */
+        var _button_number = button_number;
+        button_number = button_number || inputs[input]['previous_active_button'];
+        inputs[input]['previous_active_button'] = _button_number;
+        
+        var track = inputs[input]["button_"+button_number];
+        track.unshift({tick:count, state: button_state});
+        if (_.last(track).tick < count + options.track_length) {
+            track.pop();
+        }
+        return track;
     }
+    
+    function display_track(track) {
+        var blocks = []
+        return blocks;
+    }
+    
+    // Public ------------------------------------------------------------------
+    
+    external.tick = function() {
+        var previous_count = count;
+        count++;
+        if (count < previous_count) {
+            // reset all tracks
+        }
+    };
     
     external.event = function(data) {
         var event_handlers = {
             button_down: function(){
-                $('#button'+data.button).addClass('button_on');
+                $("#input"+data.input+"button"+data.button).addClass('button_on');  //document.getElementById('')
             },
             button_up: function() {
-                $('#button'+data.button).removeClass('button_on');
+                $("#input"+data.input+"button"+data.button).removeClass('button_on');
             },
-            /*
             note_on: function() {
-                var track = inputs[data.input][data.button];
-                track.unshift(count);
-                if (_.last(track) < count + options.track_length) {
-                    track.pop();
-                }
-                active_note_track = data.button;
+                log_track_event(data.input, ""+data.button, 1);
             },
             note_off: function() {
-                var track = inputs[data.input][active_note_track];
-                track.unshift(count);
-                active_note_track = null;
+                log_track_event(data.input, null, 0);
             },
-            */
         };
         if (_.has(event_handlers, data.event)) {
             event_handlers[data.event]();
         }
-    }
+    };
+    
+    external.display = function() {
+        /*
+        Take the track timing info and return a set of block sizes to render on the display
+        */
+        var display = Array(options.inputs.length);
+        for (var input_number=0 ; input_number < inputs.length ; input_number++) {
+            display[input_number] = Array(button_states.length);
+            for (var button_number=0 ; button_number < button_states.length ; button_number++) {
+                display[input_number][button_number] = display_track(inputs[input]["button_"+button_number]);
+            }
+        }
+        return display;
+    };
 
 }(penatonic_hero, options));
