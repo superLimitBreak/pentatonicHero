af6f204f0f3a33027160352d46ca0632b291ec35
Allan Callaghan <calaldees@gmail.com>
2015-07-24 08:08:32 +0100

    Added stub for lighting-automation render plugin

---
+++ DMXRendererPentatonicHero.py
@@ -0,0 +1,26 @@
+"""
+Plugin for use with 'lighting-automation' project
+"""
+
+from DMXBase import AbstractDMXRenderer
+
+import logging
+log = logging.getLogger(__name__)
+
+
+class DMXRendererPentatonicHero(AbstractDMXRenderer):
+
+    __name__ = 'penatonic_hero'
+
+    def __init__(self):
+        super().__init__()
+
+    def render(self, frame):
+        # do stuff
+        return self.dmx_universe
+
+    def event(self, data):
+        """
+        Network event
+        """
+        print(data)
