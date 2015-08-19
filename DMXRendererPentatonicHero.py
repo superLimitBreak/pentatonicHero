da27734032d23791c6a4f095de7d367bc79a05ae
Allan Callaghan <calaldees@gmail.com>
2015-08-19 19:08:05 +0100

    Added simplistic DMXLighting visulisation as proof of concept

--- DMXRendererPentatonicHero.py
+++ DMXRendererPentatonicHero.py
@@ -1,6 +1,7 @@
 """
 Plugin for use with 'lighting-automation' project
 """
+import operator
 
 from DMXBase import AbstractDMXRenderer
 
@@ -9,18 +10,26 @@ log = logging.getLogger(__name__)
 
 
 class DMXRendererPentatonicHero(AbstractDMXRenderer):
-
-    __name__ = 'pentatonic_hero'
+    __name__ = 'pentatonic_hero'  # The package name of network events that are to be directed to this class
 
     def __init__(self):
         super().__init__()
+        self.player_state = {1: set(), 2: set()}
 
     def render(self, frame):
-        # do stuff
+        for input_num in range(1, 2):
+            for button_num in range(5):
+                self.dmx_universe[8*button_num] = 255 if button_num in self.player_state[input_num] else 0
         return self.dmx_universe
 
     def event(self, data):
         """
-        Network event
+        Update the stored button state
         """
-        print(data)
+        button_set = self.player_state[data['input']]
+        event = data.get('event')
+        button = data.get('button')
+        if event == 'button_up':
+            button_set.discard(button)
+        if event == 'button_down':
+            button_set.add(button)
