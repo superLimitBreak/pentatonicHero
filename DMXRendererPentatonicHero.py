b86bf1569e3dcb96e08944ebc2dc1c7df4df9705
Allan Callaghan <calaldees@gmail.com>
2015-08-20 18:52:17 +0100

    Basic hack of DMX colors for player 1

--- DMXRendererPentatonicHero.py
+++ DMXRendererPentatonicHero.py
@@ -1,7 +1,6 @@
 """
 Plugin for use with 'lighting-automation' project
 """
-import operator
 
 from DMXBase import AbstractDMXRenderer
 
@@ -9,8 +8,24 @@ import logging
 log = logging.getLogger(__name__)
 
 
+COLORS = {
+    'red': (255, 0, 0),
+    'green': (0, 255, 0),
+    'blue': (0, 0, 255),
+    'yellow': (255, 127, 0),
+    'orange': (255, 90, 0),
+    'black': (0, 0, 0),
+}
+BUTTON_COLORS = tuple(COLORS[color] for color in ('red', 'green', 'yellow', 'blue', 'orange'))
+LIGHT_INDEX = [0, 8, 16, 24, 32, 40, 48, 56, 64]
+
 class DMXRendererPentatonicHero(AbstractDMXRenderer):
     __name__ = 'pentatonic_hero'  # The package name of network events that are to be directed to this class
+
+    @staticmethod
+    def set_color(dmx_universe, offset, data):
+        for index, value in enumerate(data):
+            dmx_universe[offset + index] = value
 
     def __init__(self):
         super().__init__()
@@ -19,7 +34,10 @@ class DMXRendererPentatonicHero(AbstractDMXRenderer):
     def render(self, frame):
         for input_num in range(1, 2):
             for button_num in range(5):
-                self.dmx_universe[8*button_num] = 255 if button_num in self.player_state[input_num] else 0
+                if button_num in self.player_state[input_num]:
+                    self.set_color(self.dmx_universe, LIGHT_INDEX[button_num], BUTTON_COLORS[button_num])
+                else:
+                    self.set_color(self.dmx_universe, LIGHT_INDEX[button_num], COLORS['black'])
         return self.dmx_universe
 
     def event(self, data):
