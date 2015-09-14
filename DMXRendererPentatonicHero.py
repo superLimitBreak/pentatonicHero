1a1d2ead2908f0b16286de2065468112066a802f
Allan Callaghan <calaldees@gmail.com>
2015-09-14 08:07:18 +0100

    Updated visulisation to allow player1 and 2 inputs to change their light only

--- DMXRendererPentatonicHero.py
+++ DMXRendererPentatonicHero.py
@@ -10,34 +10,49 @@ log = logging.getLogger(__name__)
 
 COLORS = {
     'red': (255, 0, 0),
-    'green': (0, 255, 0),
-    'blue': (0, 0, 255),
-    'yellow': (255, 127, 0),
-    'orange': (255, 90, 0),
+    'green': (0, 72, 0),  # (0, 255, 0)
+    'blue': (0, 0, 200),  # (0, 0, 255)
+    'yellow': (255, 72, 0),  # (255, 255, 0),
+    'orange': (255, 30, 0),
     'black': (0, 0, 0),
 }
 BUTTON_COLORS = tuple(COLORS[color] for color in ('red', 'green', 'yellow', 'blue', 'orange'))
 LIGHT_INDEX = [0, 8, 16, 24, 32, 40, 48, 56, 64]
+LIGHT_CONFIG = {
+    1: [0, 16, 8],  # floor 2 - 77, 80, 83
+    2: [40, 56, 48],  # floor 3 - 88, 91, 94
+}
+
 
 class DMXRendererPentatonicHero(AbstractDMXRenderer):
     __name__ = 'pentatonic_hero'  # The package name of network events that are to be directed to this class
 
     @staticmethod
-    def set_color(dmx_universe, offset, data):
-        for index, value in enumerate(data):
+    def set_color(dmx_universe, offset, color):
+        for index, value in enumerate(color):
             dmx_universe[offset + index] = value
+
+    @staticmethod
+    def set_player_color(dmx_universe, input_num, color):
+        for index in LIGHT_CONFIG[input_num]:
+            DMXRendererPentatonicHero.set_color(dmx_universe, index, color)
 
     def __init__(self):
         super().__init__()
         self.player_state = {1: set(), 2: set()}
 
     def render(self, frame):
-        for input_num in range(1, 2):
+        for input_num in (1, 2):
             for button_num in range(5):
-                if button_num in self.player_state[input_num]:
-                    self.set_color(self.dmx_universe, LIGHT_INDEX[button_num], BUTTON_COLORS[button_num])
-                else:
-                    self.set_color(self.dmx_universe, LIGHT_INDEX[button_num], COLORS['black'])
+                if not self.player_state[input_num]:
+                    self.set_player_color(self.dmx_universe, input_num, COLORS['black'])
+                elif button_num in self.player_state[input_num]:
+                    self.set_player_color(self.dmx_universe, input_num, BUTTON_COLORS[button_num])
+                # Reference for old logic that activated differnt lights for each button
+                #if button_num in self.player_state[input_num]:
+                #    self.set_color(self.dmx_universe, LIGHT_INDEX[button_num], BUTTON_COLORS[button_num])
+                #else:
+                #    self.set_color(self.dmx_universe, LIGHT_INDEX[button_num], COLORS['black'])
         return self.dmx_universe
 
     def event(self, data):
